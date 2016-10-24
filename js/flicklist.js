235067d49ab03e70ef39981e4051a06920310a97
Nick Summa <nicholas.summa@gmail.com>
2016-10-24 17:56:22 -0500

    Flicklist Studio 2

--- js/flicklist.js
+++ js/flicklist.js
@@ -39,7 +39,7 @@ function discoverMovies(callback) {
 
 
 /**
- * Makes an AJAX request to the /search/movie endpoint of the API, using the 
+ * Makes an AJAX request to the /search/movie endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, updates model.browseItems appropriately and then invokes
@@ -51,7 +51,17 @@ function searchMovies(searchTerm, callback) {
   // TODO 9
   // implement this function as described in the comment above
   // you can use the body of discoverMovies as a jumping off point
-
+  $.ajax({
+    url: api.root + "/search/movie",
+    data: {
+      api_key: api.token,
+      query: searchTerm
+    },
+    success: function(response) {
+      model.browseItems = response.results;
+      callback();
+      }
+    });
 
 }
 
@@ -72,7 +82,7 @@ function render() {
       .append(title)
       // TODO 3
       // give itemView a class attribute of "item-watchlist"
-
+      .attr("class", "item-watchlist");
     $("#section-watchlist ul").append(itemView);
   });
 
@@ -84,31 +94,30 @@ function render() {
       .click(function() {
         model.watchlistItems.push(movie);
         render();
-      });
+      })
+
+
       // TODO 2
       // the button should be disabled if this movie is already in
       // the user's watchlist
       // see jQuery .prop() and Array.indexOf()
-
+      .prop("disabled", model.watchlistItems.indexOf(movie) != -1);
 
     // TODO 1
     // create a paragraph containing the movie object's .overview value
     // then, in the code block below,
     // append the paragraph in between the title and the button
-
+    var overview = $("<p></p>").text(movie.overview);
 
     // append everything to itemView, along with an <hr/>
     var itemView = $("<li></li>")
       .append($("<hr/>"))
       .append(title)
+      .append(overview)
       .append(button);
 
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
-  
+
 }
-
-
-
-
